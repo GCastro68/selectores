4ca1df0503331b97243288af9416f0f3248e1bc3
GCastro68 <gcastro2805@gmail.com>
2021-10-14 13:38:42 -0600

    Carga de Fronteras

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -13,27 +13,53 @@ export class SelectorPageComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     region: ['', Validators.required],
     pais: ['', Validators.required],
+    frontera: ['', Validators.required],
   });
 
   //Llenar Selectores
   regiones: string[] = [];
   paises: PaisSmall[] = [];
+  fronteras: string[] = [];
+
+  //UI
+  cargando: boolean = false;
 
   constructor(private fb: FormBuilder, private paisesService: PaisesService) {}
 
   ngOnInit(): void {
     this.regiones = this.paisesService.regiones;
 
+    // Cuando cambia el continente
     this.miFormulario
       .get('region')
       ?.valueChanges.pipe(
         tap((_) => {
           this.miFormulario.get('pais')?.reset('');
+          this.cargando = true;
         }),
         switchMap((region) => this.paisesService.getPaisesPorRegion(region))
       )
       .subscribe((paises) => {
         this.paises = paises;
+        this.cargando = false;
+      });
+
+    // Cuando cambia el país
+    this.miFormulario
+      .get('pais')
+      ?.valueChanges.pipe(
+        tap(() => {
+          this.fronteras = [];
+          this.miFormulario.get('frontera')?.reset('');
+          this.cargando = true;
+        }),
+        switchMap((codigo) => this.paisesService.getPaisPorCodigo(codigo))
+      )
+      .subscribe((pais) => {
+        if (pais) {
+          this.fronteras = pais[0].borders || [];
+          this.cargando = false;
+        }
       });
   }
 
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { PaisSmall } from '../interfaces/paises.interface';
+import { combineLatest, Observable, of } from 'rxjs';
+import { Pais, PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaisesService {
-  private baseUrl: string = 'https://restcountries.com/v2/regionalbloc';
+  private baseUrl: string = 'https://restcountries.com/v2';
 
   private _regiones: string[] = [
     'EU (European Union)',
@@ -31,8 +32,38 @@ export class PaisesService {
   constructor(private http: HttpClient) {}
 
   getPaisesPorRegion(region: string) {
-    const url: string = `${this.baseUrl}/${region}?fields=name,alpha3Code`;
+    const url: string = `${this.baseUrl}/regionalbloc/${region}?fields=name,alpha3Code`;
 
     return this.http.get<PaisSmall[]>(url);
+  }
+
+  getPaisPorCodigo(codigo: string) {
+    if (!codigo) {
+      return of(null);
+    }
+    const url = `${this.baseUrl}/alpha?codes=${codigo}`;
+    return this.http.get<Pais[]>(url);
+    // return this.http.get<Pais>(url);
+  }
+
+  getPaisPorCodigoSmall(codigo: string) {
+    const url = `${this.baseUrl}/alpha/${codigo}?fields=name,alpha3Code`;
+
+    console.log(url);
+    return this.http.get<PaisSmall>(url);
+  }
+
+  getPaisesPorCodigos(borders: string[]) {
+    if (!borders) {
+      return of([]);
+    }
+    const peticiones: Observable<PaisSmall>[] = [];
+
+    borders.forEach((codigo) => {
+      const peticion = this.getPaisPorCodigoSmall(codigo);
+      peticiones.push(peticion);
+    });
+
+    return combineLatest(peticiones);
   }
 }
